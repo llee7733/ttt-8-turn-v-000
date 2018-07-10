38a200d41d4db9011c3bd44efbcaf0a9836d874e
Lloyd Lee <kinglloydlee@gmail.com>
2018-07-10 19:41:43 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,6 +5,8 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
+
+def input_to_index(input)
   
   def turn(board)
   puts "Please enter 1-9:"
