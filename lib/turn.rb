0d49b137abf8729d63ecacf315f953986459ac3f
Lloyd Lee <kinglloydlee@gmail.com>
2018-07-10 19:46:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,16 +16,15 @@ def valid_move?(board,index)
     else
   return false
   end
-end
   
 def position_taken?(board,index)
     if board[index] == "X" || board[index] == "O"
       true
-      elsif board[index] == " " || board[index] == "" || board[index] == nil
-+    false
-+  else
-+    false
-+  end
+    elsif board[index] == " " || board[index] == "" || board[index] == nil
+      false
+    else
+      false
+    end
   
   def turn(board)
   puts "Please enter 1-9:"
